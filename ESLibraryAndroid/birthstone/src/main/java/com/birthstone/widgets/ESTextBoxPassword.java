b5d1f87f32308c3b0b0343f2af199323226ba940
杜明悦 <Ming@dumingyuedeMacBook-Pro.local>
2017-08-21 10:40:58 +0800

    1.测试密码框显示明文的问题

--- ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/ESTextBoxPassword.java
+++ ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/ESTextBoxPassword.java
@@ -3,6 +3,8 @@ package com.birthstone.widgets;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import com.birthstone.core.helper.DataType;
+
 /**
  * 密码输入文本框
  */
@@ -10,6 +12,6 @@ public class ESTextBoxPassword extends ESTextBox {
     public ESTextBoxPassword(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.mIsRequired = true;
-//        this.setInputTypeWithDataType(DataType.Password.ordinal());
+        this.setInputTypeWithDataType(DataType.Password.ordinal());
     }
 }
