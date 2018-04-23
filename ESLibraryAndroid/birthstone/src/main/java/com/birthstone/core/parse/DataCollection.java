7407344a218c6bf79550835b4ed00091e16cc282
杜明悦 <>
2018-04-23 15:40:58 +0800

    no message

--- ESLibraryAndroid/birthstone/src/main/java/com/birthstone/core/parse/DataCollection.java
+++ ESLibraryAndroid/birthstone/src/main/java/com/birthstone/core/parse/DataCollection.java
@@ -4,121 +4,113 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class DataCollection extends LinkedList<Data> implements Serializable,Cloneable
+public class DataCollection extends LinkedList<Data> implements Serializable, Cloneable
 {
-	private static final long serialVersionUID = 5187321951128267808L;
-	public Data currentData = null;
-	private boolean isChecked;
-	private HashMap<String,Data> dataHashMap = new HashMap<>();
+    private static final long serialVersionUID = 5187321951128267808L;
+    public Data currentData = null;
+    private boolean isChecked;
+    private HashMap<String, Data> dataHashMap = new HashMap<>();
 
-	public DataCollection( )
-	{
-	}
+    public DataCollection ()
+    {
+    }
 
-	public boolean addAll(DataCollection params)
-	{
-		for(Data data:params)
-		{
-//			if(dataHashMap.containsKey(data.getName().toUpperCase()))
-//			{
-//				dataHashMap.remove(data.getName().toUpperCase());
-//			}
-			try
-			{
-				dataHashMap.put(data.getName().toUpperCase(), data);
-			}
-			catch (Exception ex)
-			{}
-		}
-		return super.addAll(params);
-	}
+    public boolean addAll (DataCollection params)
+    {
+        return super.addAll(params);
+    }
 
-	@Override
-	public boolean add (Data data)
-	{
-		if(dataHashMap.containsKey(data.getName().toUpperCase()))
-		{
-			dataHashMap.remove(data.getName().toUpperCase());
-		}
-		dataHashMap.put(data.getName().toUpperCase(),data);
-		return super.add(data);
-	}
+    @Override
+    public boolean add (Data data)
+    {
+        return super.add(data);
+    }
 
-	/**
-	 * 获取指定名称的集合
-	 * @param name 名称
-	 * **/
-	public Data get(String name)
-	{
-		if(currentData != null && currentData.Name.toUpperCase().equals(name.toUpperCase())) {
-			 return currentData;
-		}
+    /**
+     * 获取指定名称的集合
+     *
+     * @param name 名称
+     **/
+    public Data get (String name)
+    {
+        if (currentData != null && currentData.Name.toUpperCase().equals(name.toUpperCase()))
+        {
+            return currentData;
+        }
 
-		currentData = dataHashMap.get(name.toUpperCase());
-//		int size = this.size();
-//		 for(int i = 0; i < size; i++)
-//		 {
-//			 if(this.get(i).Name.toUpperCase().equals(name.toUpperCase()))
-//			 {
-//				 data = this.get(i);
-//				 CurrentData = data;
-//				 break;
-//			 }
-//		 }
-		 return currentData;
-	}
+        int size = this.size();
+        if(dataHashMap.size()!= size)
+        {
+            dataHashMap.clear();
+            for (int i = 0; i < size; i++)
+            {
+                try
+                {
+                    dataHashMap.put(this.get(i).getName().toUpperCase(), this.get(i));
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+        currentData = dataHashMap.get(name.toUpperCase());
+        return currentData;
+    }
 
-	@Override
-	public boolean remove (Object o)
-	{
-		currentData=null;
-		Data data = (Data)o;
-		if(dataHashMap.containsKey(data.getName()))
-		{
-			dataHashMap.remove(data.getName());
-		}
-		return super.remove(o);
-	}
+    @Override
+    public boolean remove (Object o)
+    {
+        currentData = null;
+        Data data = (Data) o;
+        if (dataHashMap.containsKey(data.getName()))
+        {
+            dataHashMap.remove(data.getName());
+        }
+        return super.remove(o);
+    }
 
-	@Override
-	public void clear ()
-	{
-		dataHashMap.clear();
-		super.clear();
-	}
+    @Override
+    public void clear ()
+    {
+        dataHashMap.clear();
+        super.clear();
+    }
 
-	/**
-	 * 获取当前集合是否选中状态
-	 * **/
-	public boolean isChecked() {
-		return isChecked;
-	}
+    /**
+     * 获取当前集合是否选中状态
+     **/
+    public boolean isChecked ()
+    {
+        return isChecked;
+    }
 
-	/**
-	 * 设置当前集合选中状态
-	 * @param isChecked 状态
-	 * **/
-	public void setChecked(boolean isChecked) {
-		this.isChecked = isChecked;
-	}
+    /**
+     * 设置当前集合选中状态
+     *
+     * @param isChecked 状态
+     **/
+    public void setChecked (boolean isChecked)
+    {
+        this.isChecked = isChecked;
+    }
 
-	@Override
-	public Object clone ()
-	{
-		DataCollection dataCollection = null;
-		try
-		{
-			dataCollection = (DataCollection)super.clone();
-			dataCollection.clear();
-			for(Data data:this)
-			{
-				dataCollection.add((Data)data.clone());
-			}
-		}
-		catch (Exception ex)
-		{
+    @Override
+    public Object clone ()
+    {
+        DataCollection dataCollection = null;
+        try
+        {
+            dataCollection = (DataCollection) super.clone();
+            dataCollection.clear();
+            for (Data data : this)
+            {
+                dataCollection.add((Data) data.clone());
+            }
+        }
+        catch (Exception ex)
+        {
 
-		}
-		return dataCollection;
-	}
+        }
+        return dataCollection;
+    }
 }
