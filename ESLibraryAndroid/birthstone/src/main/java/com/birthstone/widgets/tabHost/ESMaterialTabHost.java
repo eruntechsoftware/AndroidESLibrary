80f9d78a69220931de70e94ba978e835ee223a77
dumingyue <dumingyue@xinruan.com>
2018-09-12 15:18:04 +0800

    no message

--- ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/tabHost/ESMaterialTabHost.java
+++ ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/tabHost/ESMaterialTabHost.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -16,6 +15,7 @@ import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
 import com.birthstone.R;
 import com.birthstone.base.activity.Activity;
+import com.birthstone.base.helper.ActivityHelper;
 
 import java.util.ArrayList;
 
@@ -268,14 +268,14 @@ public class ESMaterialTabHost extends LinearLayout implements View.OnClickListe
 	{
 		//先获取屏幕宽度
 		int screenW = 0;
-		//		if (this.mFragment != null)
-		//		{
-		//			screenW = ActivityHelper.getActivityWidth(this.mFragment.getActivity());
-		//		}
-		//		if (this.mFragmentActivity != null)
-		//		{
-		//			screenW = ActivityHelper.getActivityWidth(this.mFragmentActivity);
-		//		}
+		if (this.mFragment != null)
+		{
+			screenW = ActivityHelper.getActivityWidth(this.mFragment.getActivity());
+		}
+		if (this.mFragmentActivity != null)
+		{
+			screenW = ActivityHelper.getActivityWidth(this.mFragmentActivity);
+		}
 		//每段title的宽度
 		offset = screenW / fragmentList.size();
 
@@ -362,88 +362,88 @@ public class ESMaterialTabHost extends LinearLayout implements View.OnClickListe
 		}
 	}
 
-//	/**
-//	 * 中断事件
-//	 */
-//	@Override
-//	public boolean onInterceptTouchEvent(MotionEvent ev)
-//	{
-//		return true;
-//	}
-//
-//	/**
-//	 * 分发事件
-//	 */
-//	@Override
-//	public boolean dispatchTouchEvent(MotionEvent ev)
-//	{
-//		return super.dispatchTouchEvent(ev);
-//	}
-//
-//	/**
-//	 * 实现多个ListView控件同时触发事件
-//	 */
-//	@Override
-//	public boolean onTouchEvent(MotionEvent event)
-//	{
-//
-//		int width = getWidth() / getChildCount();
-//		int height = getHeight();
-//		int count = getChildCount();
-//
-//		float eventX = event.getX();
-//
-//		if (eventX < width)
-//		{    // 滑动左边的Fragment
-//			event.setLocation(width / 2, event.getY());
-//			getChildAt(0).dispatchTouchEvent(event);//移动位置后，分发事件
-//			return true;
-//
-//		}
-//		else if (eventX > width && eventX < 2 * width)
-//		{ //滑动中间的 listView
-//			float eventY = event.getY();
-//			if (eventY < height / 2)
-//			{
-//				event.setLocation(width / 2, event.getY());
-//				for (int i = 0; i < count; i++)
-//				{
-//					View child = getChildAt(i);
-//					try
-//					{
-//						child.dispatchTouchEvent(event);
-//					}
-//					catch(Exception e)
-//					{
-//						e.printStackTrace();
-//					}
-//
-//				}
-//				return true;
-//			}
-//			else if (eventY > height / 2)
-//			{
-//				event.setLocation(width / 2, event.getY());
-//				try
-//				{
-//					getChildAt(1).dispatchTouchEvent(event);
-//				}
-//				catch(Exception e)
-//				{
-//					e.printStackTrace();
-//				}
-//				return true;
-//			}
-//		}
-//		else if (eventX > 2 * width)
-//		{
-//			event.setLocation(width / 2, event.getY());
-//			getChildAt(2).dispatchTouchEvent(event);
-//			return true;
-//		}
-//
-//		return true;
-//	}
+	//	/**
+	//	 * 中断事件
+	//	 */
+	//	@Override
+	//	public boolean onInterceptTouchEvent(MotionEvent ev)
+	//	{
+	//		return true;
+	//	}
+	//
+	//	/**
+	//	 * 分发事件
+	//	 */
+	//	@Override
+	//	public boolean dispatchTouchEvent(MotionEvent ev)
+	//	{
+	//		return super.dispatchTouchEvent(ev);
+	//	}
+	//
+	//	/**
+	//	 * 实现多个ListView控件同时触发事件
+	//	 */
+	//	@Override
+	//	public boolean onTouchEvent(MotionEvent event)
+	//	{
+	//
+	//		int width = getWidth() / getChildCount();
+	//		int height = getHeight();
+	//		int count = getChildCount();
+	//
+	//		float eventX = event.getX();
+	//
+	//		if (eventX < width)
+	//		{    // 滑动左边的Fragment
+	//			event.setLocation(width / 2, event.getY());
+	//			getChildAt(0).dispatchTouchEvent(event);//移动位置后，分发事件
+	//			return true;
+	//
+	//		}
+	//		else if (eventX > width && eventX < 2 * width)
+	//		{ //滑动中间的 listView
+	//			float eventY = event.getY();
+	//			if (eventY < height / 2)
+	//			{
+	//				event.setLocation(width / 2, event.getY());
+	//				for (int i = 0; i < count; i++)
+	//				{
+	//					View child = getChildAt(i);
+	//					try
+	//					{
+	//						child.dispatchTouchEvent(event);
+	//					}
+	//					catch(Exception e)
+	//					{
+	//						e.printStackTrace();
+	//					}
+	//
+	//				}
+	//				return true;
+	//			}
+	//			else if (eventY > height / 2)
+	//			{
+	//				event.setLocation(width / 2, event.getY());
+	//				try
+	//				{
+	//					getChildAt(1).dispatchTouchEvent(event);
+	//				}
+	//				catch(Exception e)
+	//				{
+	//					e.printStackTrace();
+	//				}
+	//				return true;
+	//			}
+	//		}
+	//		else if (eventX > 2 * width)
+	//		{
+	//			event.setLocation(width / 2, event.getY());
+	//			getChildAt(2).dispatchTouchEvent(event);
+	//			return true;
+	//		}
+	//
+	//		return true;
+	//	}
 
 	/**
 	 * 移除所有视图
